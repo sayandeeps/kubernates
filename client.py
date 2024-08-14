a23c4d7e128b418ba39dcd8f0c34ef9d133803b1
Sayandeep <sayandeep11@gmail.com>
2024-08-14 15:27:51 +0530

    implemented with mini kubes

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 import httpx
 import asyncio
 import time
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 
 # Define the servers
 servers = [
-    {"name": "Server 1", "url": "http://127.0.0.1:8000/hi"},
-    {"name": "Server 2", "url": "http://localhost:8001/hi"}
+    {"name": "Server 1", "url": "http://127.0.0.1:61512/hi"},
+    {"name": "Server 2", "url": "http://127.0.0.1:61824/hi"}
 ]
 
 # Define the timeout period (in seconds)
@@ -19,12 +23,14 @@ async def client():
             server = servers[server_index]
             try:
                 response = await client.post(server["url"], content="hi", timeout=timeout)
-                print(f"Received response from {server['name']} which is {server['url']}: {response.text}")
+                logging.info(f"Received response from {server['name']} which is {server['url']}: {response.text}")
             except httpx.RequestError as e:
-                print(f"Error sending request to {server['name']}: {e}")
-                print(f"{server['name']} which is {server['url']} is down, skipping to next server")
+                logging.error(f"Error sending request to {server['name']}: {e}")
+                logging.warning(f"{server['name']} which is {server['url']} is down, skipping to next server")
+
+
             except asyncio.TimeoutError:
-                print(f"Timeout error while sending request to {server['name']}, skipping to next server")
+                logging.warning(f"Timeout error while sending request to {server['name']}, skipping to next server")
             server_index = (server_index + 1) % len(servers)  # round-robin
             await asyncio.sleep(1)  # wait for 1 second before sending the next request
 
