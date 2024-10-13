1502ae9df72105a27d246393139c4a8a3186773f
Sandi <huejass6988@gmail.com>
2024-10-13 20:56:19 +0000

    new changes

--- client.py
+++ client.py
@@ -8,8 +8,8 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=lo
 
 # Define the servers
 servers = [
-    {"name": "Server 1", "url": "http://10.1.1.11:30080/hi"},
-    {"name": "Server 2", "url": "http://127.0.0.1:61824/hi"}
+    {"name": "Server 1", "url": "http://172.16.229.140:30080/hi"},
+    {"name": "Server 2", "url": "http://172.16.229.140:30081/hi"}
 ]
 
 # Define the timeout period (in seconds)
